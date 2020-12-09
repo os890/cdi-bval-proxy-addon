ac87de9f1871e6c48a5dcb10c8fd2316bba3a4f8
os890 <open.source.890@gmail.com>
2020-12-09 01:54:28 +0100

    added broken validations with bval v2.0.5

---
+++ owb/src/test/java/org/os890/proxy/addon/owb/test/BrokenOwbProxyValidationTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.os890.proxy.addon.owb.test;
+
+import org.apache.bval.jsr.ApacheValidatorFactory;
+import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(CdiTestRunner.class)
+public class BrokenOwbProxyValidationTest {
+    @Inject
+    private TestProxiedBean proxiedBean;
+
+    @Test
+    public void brokenProxyValidation() {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        Set<ConstraintViolation<TestProxiedBean>> violations = validator.validate(proxiedBean);
+        assertThat(violations.size(), is(1));
+    }
+}
